061de53382819538cacf711eae1051d038d2fdbd
Cassia Carvalho <contato.cassia.carvalho@gmail.com>
2023-09-12 22:39:36 -0300

    attMenuResponsivo

--- app.js
+++ app.js
@@ -43,3 +43,20 @@ function validarFormulario() {
 
     return true;
 }
+
+//botao-menu
+
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu-mobile');
+let overlay = document.getElementById('overlay-menu');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
