3acca7cb34e15540c69628f36b256316d1d7464b
Cássia Carvalho <contato.cassia.carvalho@gmail.com>
2024-01-16 21:57:16 -0300

    :bug: fix: atualizando php

--- envia.php
+++ envia.php
@@ -4,20 +4,29 @@
     $telefone = addcslashes($_POST['telefone']);
     $assunto = addcslashes($_POST['assunto']);
     $mensagem = addcslashes($_POST['mensagem']);
+    $data_envio = date('d/m/Y');
+    $hora_envio = date('H:i:s');
 
+    //corpo email
+    $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Telefone: </b>$telefone</p>
+      <p><b>Assunto: </b>$assunto</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+
+  //Para onde será enviado
     $para = "contato.cassia.carvalho@gmail.com";
     $titulo = "Mensagem do portfolio - Contato";
 
-    $corpo = "Nome: ".$nome."\n"."Email: ".$email."\n"."Telefone: ".$telefone."\n"."Assunto: ".$assunto."\n"."Mensagem: ".$mensagem;
-
     $cabeca = "From: cahcarvalh@hotmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
-    if(mail($para,$titulo,$corpo,$cabeca))
-    {
-        echo("E-mail enviado com sucesso!");
-    }
-    else
-    {
-        echo("Houve um erro ao enviar o email. Tente novamente mais tarde!");
-    }
+    mail($para,$titulo,$arquivo,$cabeca);
+    
+    echo "<meta http-equiv='refresh' content='10;URL=../contato.html'>";
+    
 ?>
